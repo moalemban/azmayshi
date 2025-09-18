581a25c237c2764c56284e304260a36d4daed777
معلم بان <moalemban@gmail.com>
2025-09-18 13:40:18 +0000

    و   <img alt="تبدیلا لوگو" loading="lazy" width="48" height="48" decodin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from '@/components/theme-provider';
 export const metadata: Metadata = {
   title: 'تبدیلا | دستیار هوشمند شما',
   description: 'دستیار هوشمند شما برای انواع محاسبات و تبدیل واحدها',
+  icons: {
+    icon: 'https://uploadkon.ir/uploads/886e18_252fb04e3a-9e9c-44da-8d7e-7a0345be973aa4-copy.png',
+  },
 };
 
 export default function RootLayout({
