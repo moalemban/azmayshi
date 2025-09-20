d815a73251130effed29214775978b17a11070f2
معلم بان <moalemban@gmail.com>
2025-09-20 17:30:51 +0000

    فاو ایکون هنوزز تغییر نکرده لطفا تغییرش بده به https://uploadkon.ir/uplo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="fa" dir="rtl" suppressHydrationWarning>
       <head>
          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" />
+         <link rel="icon" href="https://uploadkon.ir/uploads/cf7220_252fb04e3a-9e9c-44da-8d7e-7a0345be973aa4-copy.png" type="image/png" />
       </head>
       <body className={vazirmatn.variable}>
         <ThemeProvider
