8bf8272c1c36c4309fc5adece59158290d05cf85
معلم بان <moalemban@gmail.com>
2025-09-20 11:34:13 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,21 @@
-
-import createNextPwa from '@ducanh2912/next-pwa';
-
-const withPWA = createNextPwa({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'uploadkon.ir' },
-      { protocol: 'https', hostname: 'cdn3d.iconscout.com' },
-      { protocol: 'https', hostname: 'png.pngtree.com' },
-      { protocol: 'https', hostname: 'ice.ir' },
+      {
+        protocol: 'https',
+        hostname: 'static.idpay.ir',
+        port: '',
+        pathname: '/banks/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploadkon.ir',
+        port: '',
+        pathname: '/uploads/**',
+      },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    // Ignore .map files
-    config.module.rules.push({
-      test: /\.map$/,
-      use: 'ignore-loader',
-    });
-
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
