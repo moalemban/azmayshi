eba650c82d94bbc3193a0389cc03ea044602b760
معلم بان <moalemban@gmail.com>
2025-09-20 09:32:22 +0000

    Try fixing this error: `Runtime Error: ModuleParseError: Module parse fa

--- next.config.mjs
+++ next.config.mjs
@@ -1,42 +1,35 @@
-import withPWAInit from "@ducanh2912/next-pwa";
 
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+import createNextPwa from '@ducanh2912/next-pwa';
+
+const withPWA = createNextPwa({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'uploadkon.ir',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn3d.iconscout.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'ice.ir',
-      },
-       {
-        protocol: 'https',
-        hostname: 'png.pngtree.com',
-      }
+      { protocol: 'https', hostname: 'uploadkon.ir' },
+      { protocol: 'https', hostname: 'cdn3d.iconscout.com' },
+      { protocol: 'https', hostname: 'png.pngtree.com' },
+      { protocol: 'https', hostname: 'ice.ir' },
     ],
   },
   webpack: (config, { isServer }) => {
-    // Fix for "Module parse failed: Unexpected token" error with chrome-aws-lambda
+    // Ignore .map files
     config.module.rules.push({
       test: /\.map$/,
-      use: 'null-loader',
+      use: 'ignore-loader',
     });
 
-    // Another way to potentially handle it if the above doesn't work is to exclude it.
-    // This is generally not needed if null-loader is used.
-    // config.externals.push('chrome-aws-lambda');
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
 
     return config;
   },
