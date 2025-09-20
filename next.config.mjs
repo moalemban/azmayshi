45169b636e6040d5f8963ee041eda2297d3b02fb
معلم بان <moalemban@gmail.com>
2025-09-20 14:40:26 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'static.idpay.ir',
-                port: '',
-                pathname: '/banks/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'uploadkon.ir',
-                port: '',
-                pathname: '/uploads/**',
-            }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.tgju.org',
+        port: '',
+        pathname: '/images/crypto/icons/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'uploadkon.ir',
+        port: '',
+        pathname: '/uploads/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
