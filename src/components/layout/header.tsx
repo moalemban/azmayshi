ff2e587821c3ca64d4527de7f0c2912a3625a53e
معلم بان <moalemban@gmail.com>
2025-09-20 17:31:37 +0000

    توی موبایل باید ساعت رو هم نشون بده توی هدر

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -35,7 +35,7 @@ const LiveClock = () => {
     });
 
     return (
-        <div className="hidden md:flex items-center space-x-2 space-x-reverse bg-background/50 rounded-full px-4 py-2">
+        <div className="flex items-center space-x-2 space-x-reverse bg-background/50 rounded-full px-4 py-2">
             <Clock className="w-5 h-5 text-primary animate-pulse" />
             <span className="text-primary text-base font-display font-semibold text-glow">{formattedTime}</span>
         </div>
